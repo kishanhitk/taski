95741ae713ae8bf7b2adcbca75d8e25fb98cd14a
kishanhitk <kishansharma1231@gmail.com>
2024-08-28 21:43:58 +0530

    init(backend)

---
+++ backend/src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Task Manager Backend!");
+});
+
+app.listen(port, () => {
+  console.log(
+    `Server running on port ${port}. Navigate to http://localhost:${port}`
+  );
+});
