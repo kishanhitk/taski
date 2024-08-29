c0fb060718368db9c9ebb98f0d3f13c3705532e8
kishanhitk <kishansharma1231@gmail.com>
2024-08-29 13:51:24 +0530

    feat(fe): add shadcn

--- website/src/app/dashboard/UserInfo.tsx
+++ website/src/app/dashboard/UserInfo.tsx
@@ -1,16 +1,14 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
 
 export default function UserInfo({ email }) {
   return (
     <div>
       {email}
-      <button
-        onClick={() => signOut()}
-        className="ml-4 px-4 py-2 bg-red-500 text-white rounded"
-      >
+      <Button variant="outline" onClick={() => signOut()}>
         Logout
-      </button>
+      </Button>
     </div>
   );
 }
