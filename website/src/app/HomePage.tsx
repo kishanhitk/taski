b619ac77d8689e69f35939642bde83c52df524b5
kishanhitk <kishansharma1231@gmail.com>
2024-08-30 08:45:21 +0530

    feat(fe): improve padding

--- website/src/app/HomePage.tsx
+++ website/src/app/HomePage.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 md:p-16 lg:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-center container pt-12 md:pt-0 ">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
